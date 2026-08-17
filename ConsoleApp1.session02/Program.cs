3558f443592e600b88ba6d1a05e417697ae4fccd
Thúy Ngân <Thy Ngn@DESKTOP-SQVAQC5>
2026-08-17 15:08:28 +0700

    Add project files.

---
+++ ConsoleApp1.session02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;  
+
+namespace ConsoleApp1.session02
+{
+    internal class Ex01
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter your YOB: ");
+            int yob = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter your height (cm): ");
+            float height = Convert.ToSingle(Console.ReadLine());
+           
+            Console.WriteLine($"Họ và tên: {name}, Tuổi: {age}");
+            //Console.WriteLine("họ và tên: {0}, Age: {1}", name, age);
+
+            string greating = "\n\n\tHola \"moi nguoi\"";
+            Console.WriteLine(greating);
+
+            Console.ReadKey();
+        }
+    }
+}
